741840a7c48080922e491da9cd6cc6202e188a3e
giggityuser <giggityuser@users.noreply.github.com>
2014-03-28 18:15:27 +0530

    Update doony.js

--- doony.js
+++ doony.js
@@ -425,7 +425,7 @@
 
     var doonyTitleLink = $("#top-panel a").first();
     //var domain = getSubdomain(window.location.hostname);
-    doonyTitleLink.html("<div id='doony-title'>" + "<img src='http://iwe.cisco.com/image/image_gallery?img_id=838521775'>" + "</div>");
+    doonyTitleLink.html("<div id='doony-title'>" + "<img src='http://iwe.cisco.com/image/image_gallery?img_id=838521876'>" + "</div>");
 
     var color = colors[Math.abs(hashCode(domain)) % colors.length];
     $("#top-panel").css('background-color', color);
